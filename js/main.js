df40070fc11d2b53c2573e184addc9621d518489
bahaerdogan <baha-erdogan@hotmail.com>
2024-06-14 12:05:32 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,12 +10,12 @@
   })
 
   // Preloader
-  $(window).on('load', function () {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
-        $(this).remove();
-      });
-    }
+$(window).on('load', function () {
+	if ($('#preloader').length) {
+	  $('#preloader').delay(100).fadeOut('slow', function () {
+		$(this).remove();
+	  });
+	}
   });
 
   // Back to top button
