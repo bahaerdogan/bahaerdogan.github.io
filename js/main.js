f57d657e1712e114a009166e00df0103eb41bbf3
Baha Erdoğan <110889035+bahaerdogan@users.noreply.github.com>
2025-03-02 16:26:43 +0300

    js ve index güncellemesi (geri alınabilir, cursor)

--- js/main.js
+++ js/main.js
@@ -1,51 +1,65 @@
 (function ($) {
 	"use strict";
 	var nav = $('nav');
-  var navHeight = nav.outerHeight();
-  
-  $('.navbar-toggler').on('click', function() {
-    if( ! $('#mainNav').hasClass('navbar-reduce')) {
-      $('#mainNav').addClass('navbar-reduce');
-    }
-  })
+	var navHeight = nav.outerHeight();
+	
+	// Navbar toggle handler with improved functionality
+	$('.navbar-toggler').on('click', function() {
+		if (!$('#mainNav').hasClass('navbar-reduce')) {
+			$('#mainNav').addClass('navbar-reduce');
+		}
+	});
 
-  // Preloader
-  $(window).on('load', function () {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
-        $(this).remove();
-      });
-    }
-  });
+	// Preloader with improved fade animation
+	$(window).on('load', function () {
+		if ($('#preloader').length) {
+			$('#preloader').delay(100).fadeOut('slow', function () {
+				$(this).remove();
+			});
+		}
+	});
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-    return false;
-  });
+	// Back to top button with smooth animation
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 100) {
+			$('.back-to-top').fadeIn('slow');
+		} else {
+			$('.back-to-top').fadeOut('slow');
+		}
+	});
 
-	/*--/ Star ScrollTop /--*/
-	$('.scrolltop-mf').on("click", function () {
+	$('.back-to-top').click(function(e){
+		e.preventDefault();
+		$('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+		return false;
+	});
+
+	// Improved scroll to top functionality
+	$('.scrolltop-mf').on("click", function (e) {
+		e.preventDefault();
 		$('html, body').animate({
 			scrollTop: 0
 		}, 1000);
 	});
 
-	/*--/ Star Counter /--*/
-	$('.counter').counterUp({
-		delay: 15,
-		time: 2000
+	// Counter initialization with viewport check
+	const initCounter = () => {
+		if ($('.counter').length) {
+			$('.counter').counterUp({
+				delay: 15,
+				time: 2000
+			});
+		}
+	};
+	
+	// Initialize counter when element is in viewport
+	$(window).on('scroll', function() {
+		initCounter();
 	});
 
-	/*--/ Star Scrolling nav /--*/
-	$('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
+	// Enhanced smooth scrolling navigation
+	$('a.js-scroll[href*="#"]:not([href="#"])').on("click", function (e) {
+		e.preventDefault();
 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -58,60 +72,99 @@
 		}
 	});
 
-	// Closes responsive menu when a scroll trigger link is clicked
+	// Responsive menu handling
 	$('.js-scroll').on("click", function () {
 		$('.navbar-collapse').collapse('hide');
 	});
 
-	// Activate scrollspy to add active class to navbar items on scroll
-	$('body').scrollspy({
-		target: '#mainNav',
-		offset: navHeight
-	});
-	/*--/ End Scrolling nav /--*/
+	// Enhanced scrollspy with error handling
+	try {
+		$('body').scrollspy({
+			target: '#mainNav',
+			offset: navHeight
+		});
+	} catch (e) {
+		console.warn('Scrollspy initialization failed:', e);
+	}
 
-	/*--/ Navbar Menu Reduce /--*/
+	// Improved navbar animation
 	$(window).trigger('scroll');
 	$(window).on('scroll', function () {
-		var pixels = 50; 
+		var pixels = 50;
 		var top = 1200;
-		if ($(window).scrollTop() > pixels) {
-			$('.navbar-expand-md').addClass('navbar-reduce');
-			$('.navbar-expand-md').removeClass('navbar-trans');
+		var scroll = $(window).scrollTop();
+
+		if (scroll > pixels) {
+			$('.navbar-expand-md').addClass('navbar-reduce').removeClass('navbar-trans');
 		} else {
-			$('.navbar-expand-md').addClass('navbar-trans');
-			$('.navbar-expand-md').removeClass('navbar-reduce');
+			$('.navbar-expand-md').addClass('navbar-trans').removeClass('navbar-reduce');
 		}
-		if ($(window).scrollTop() > top) {
+
+		if (scroll > top) {
 			$('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
 		} else {
 			$('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
 		}
 	});
 
-	/*--/ Star Typed /--*/
-	if ($('.text-slider').length == 1) {
-    var typed_strings = $('.text-slider-items').text();
-		var typed = new Typed('.text-slider', {
-			strings: typed_strings.split(','),
-			typeSpeed: 80,
+	// Enhanced text slider initialization
+	const initTextSlider = () => {
+		if ($('.text-slider').length === 1) {
+			try {
+				var typed_strings = $('.text-slider-items').text();
+				var typed = new Typed('.text-slider', {
+					strings: typed_strings.split(','),
+					typeSpeed: 80,
+					loop: true,
+					backDelay: 1100,
+					backSpeed: 30,
+					smartBackspace: true,
+					showCursor: true,
+					cursorChar: '|'
+				});
+			} catch (e) {
+				console.warn('Typed.js initialization failed:', e);
+			}
+		}
+	};
+	initTextSlider();
+
+	// Improved testimonial carousel
+	if ($('#testimonial-mf').length) {
+		$('#testimonial-mf').owlCarousel({
+			margin: 20,
+			autoplay: true,
+			autoplayTimeout: 4000,
+			autoplayHoverPause: true,
+			responsive: {
+				0: {
+					items: 1,
+				},
+				768: {
+					items: 2,
+				},
+				992: {
+					items: 3,
+				}
+			},
+			nav: true,
+			dots: true,
 			loop: true,
-			backDelay: 1100,
-			backSpeed: 30
+			navText: [
+				'<i class="ion-chevron-left"></i>',
+				'<i class="ion-chevron-right"></i>'
+			]
 		});
 	}
 
-	/*--/ Testimonials owl /--*/
-	$('#testimonial-mf').owlCarousel({
-		margin: 20,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
-			}
-		}
+	// Handle window resize events
+	let resizeTimer;
+	$(window).on('resize', function() {
+		clearTimeout(resizeTimer);
+		resizeTimer = setTimeout(function() {
+			navHeight = nav.outerHeight();
+			$('body').scrollspy('refresh');
+		}, 250);
 	});
 
 })(jQuery);
