18c9766ad9defbd2f7cae122d0f5c172072819a6
Baha Erdoğan <baha-erdogan@hotmail.com>
2025-04-24 10:01:18 +0300

    navbarupdate

---
+++ js/blog.js
@@ -0,0 +1,49 @@
+// Smooth scrolling for all links
+$('a.js-scroll[href*="#"]:not([href="#"])').on('click', function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: (target.offset().top - 74)
+      }, 1000, "easeInOutExpo");
+      return false;
+    }
+  }
+});
+
+// Closes responsive menu when a scroll trigger link is clicked
+$('.js-scroll').on('click', function() {
+  $('.navbar-collapse').collapse('hide');
+});
+
+// Activate scrollspy to add active class to navbar items on scroll
+$('body').scrollspy({
+  target: '#mainNav',
+  offset: 74
+});
+
+// Collapse Navbar
+var navbarCollapse = function() {
+  if ($("#mainNav").offset().top > 100) {
+    $("#mainNav").addClass("navbar-shrink");
+  } else {
+    $("#mainNav").removeClass("navbar-shrink");
+  }
+};
+// Collapse now if page is not at top
+navbarCollapse();
+// Collapse the navbar when page is scrolled
+$(window).on('scroll', navbarCollapse);
+
+// Handle hash navigation when loading the page
+$(document).ready(function() {
+  if (window.location.hash) {
+    var target = $(window.location.hash);
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: (target.offset().top - 74)
+      }, 1000, "easeInOutExpo");
+    }
+  }
+}); 
